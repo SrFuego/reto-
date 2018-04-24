bad510d24056e18634a3bb70443c9adde25e3853
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-04-24 00:47:15 -0500

    ADD first commit

---
+++ gulpfile.js
@@ -0,0 +1,165 @@
+const gulp = require("gulp");
+const del = require("del");
+const shell = require("gulp-shell");
+const plumber = require("gulp-plumber");
+const stylus = require("gulp-stylus");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const pug = require("gulp-pug");
+const watch = require("gulp-watch");
+const server = require("gulp-server-livereload");
+
+
+// const connect = require('gulp-connect');
+// const inject = require('gulp-inject');
+// const wiredep = require('wiredep').stream;
+// const nib = require('nib');
+// const minifyCSS = require('gulp-csso');
+
+
+//----------------------------------------------------||
+//  DELETE EVERY FILE, CREATE FOLDERS AND COPY FILES  ||
+//----------------------------------------------------||
+
+//Delete every file from ./public/ before compiling
+gulp.task("remove", function () {
+    return del(["public/**/*"]);
+});
+
+//Create folders on ./public
+gulp.task("create", ["remove"], function () {
+    return gulp.src("*.js", {read: false})
+        .pipe(shell([
+            "mkdir -p public/assets public/assets/icons public/assets/images public/assets/fonts public/assets/others public/css public/scripts public/scripts/vendor"
+        ]));
+});
+
+//Copy files from assets and fonts
+gulp.task("copy", ["create"], function () {
+    gulp.src(["source/assets/**/*"])
+        .pipe(gulp.dest("public/assets/"));
+});
+
+//Globalize remove, create and copy tasks
+gulp.task("start", ["remove", "create", "copy"]);
+
+
+//--------------------------------------||
+//  STYLUS, AUTOPREFIX AND MINIFY TASK  ||
+//--------------------------------------||
+
+//Compile stylus files
+gulp.task("stylus", ["start"], function () {
+    return gulp.src("source/styles/main.styl")
+        .pipe(plumber())
+        .pipe(stylus({
+            "include css": true
+        }))
+        .pipe(autoprefixer({
+            browsers: ["> 5%"],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: "ie9"}))
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("public/css"));
+});
+
+//Globalize stylus task
+gulp.task("styles", ["stylus"]);
+
+
+//----------------------------------||
+//  CONCATENATE VENDORS, UGLIFY JS  ||
+//----------------------------------||
+
+//Process libraries js
+gulp.task("concatenate", ["start"], function () {
+    return gulp.src(["source/scripts/vendor/*.js"])
+        .pipe(plumber())
+        .pipe(concat("vendor.js"))
+        .pipe(uglify())
+        .pipe(rename("vendor.min.js"))
+        .pipe(gulp.dest("public/scripts/"));
+});
+
+//Process own js
+gulp.task("ugly", ["start"], function () {
+    return gulp.src("source/scripts/app/*.js")
+        .pipe(plumber())
+        .pipe(concat("main.js"))
+        .pipe(uglify())
+        .pipe(rename("main.min.js"))
+        .pipe(gulp.dest("public/scripts/"));
+});
+
+//Globalize scripts task
+gulp.task("scripts", ["concatenate", "ugly"]);
+
+
+//---------||
+//  VIEWS  ||
+//---------||
+
+//Process pug templates
+gulp.task("pug", ["start"], function () {
+    return gulp.src("source/templates/*.pug")
+        .pipe(plumber())
+        .pipe(pug())
+        .pipe(gulp.dest("public/"));
+});
+
+
+//------------||
+//  WATCHERS  ||
+//------------||
+gulp.task("watch", ["webserver"], function () {
+    gulp.watch("source/scripts/app/*.js", ["ugly"]);
+    gulp.watch("source/styles/*.styl", ["stylus"]);
+    gulp.watch("source/templates/*.pug", ["pug"]);
+    watch("source/assets/**/*", function () {
+        gulp.start("copy");
+    });
+    watch("source/scripts/vendor/*.js", function () {
+        gulp.start("concatenate");
+    });
+});
+
+
+//-----------------------||
+//  LIVERELOAD & SERVER  ||
+//-----------------------||
+gulp.task("webserver", ["styles", "scripts", "pug"], function () {
+    gulp.src("./public")
+        .pipe(server({
+            livereload: true,
+            defaultFile: "index.html",
+            // directoryListing: {
+            //     enable: true,
+            //     path: './public'
+            // },
+            // fallback: 'index.html',
+            open: true
+        }));
+});
+
+
+//---------||
+//  SERVE  ||
+//---------||
+gulp.task("default", ["start", "styles", "scripts", "pug", "webserver", "watch"]);
+
+
+// // Watch files to compile and reload
+// gulp.task('watch', function () {
+//     gulp.watch(['src/templates/*.pug'], ['inject', 'html']);
+//     gulp.watch(['src/styles/*.styl'], ['css-dev']);
+//     gulp.watch(['./bower.json'], ['wiredep']);
+// });
+//
+//
+// // gulp.task('default', [ 'html', 'css', 'js' ]);
+// gulp.task('default', ['server', 'watch']);
+// gulp.task('build', ['css-prod', 'html']);
